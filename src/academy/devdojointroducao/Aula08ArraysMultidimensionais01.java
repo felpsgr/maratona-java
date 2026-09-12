9025cc8b09acc29faee2dbb56ce9abde5bcc8b50
felpsgr <felipepipocadoce@gmail.com>
2026-09-12 15:06:53 -0400

    mais aula

--- src/academy/devdojointroducao/Aula08ArraysMultidimensionais01.java
+++ src/academy/devdojointroducao/Aula08ArraysMultidimensionais01.java
@@ -2,6 +2,26 @@ package academy.devdojointroducao;
 
 public class Aula08ArraysMultidimensionais01 {
     static void main(String[] args) {
-        int[][] dias = new int[12][];
+        int[][] dias = new int[3][3];
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 30;
+
+        dias[1][0] = 31;
+        dias[1][1] = 28;
+        dias[1][2] = 30;
+
+        for (int i = 0; i < dias.length; i++) {
+            for (int j = 0; j < dias[i].length; j++) {
+                System.out.println(dias[i][j]);
+            }
+        }
+        System.out.println("--------------------------");
+
+        for (int[] arrBase: dias) {
+            for (int num: arrBase) {
+                System.out.println(num);
+            }
+        }
     }
 }
